69f28bc30cdac002915d614e3bad0159f4d0444e
michelebraidotti <michele.braidotti@gmail.com>
2017-08-24 15:04:53 +0200

    Inital import

---
+++ src/main/java/org/genomesmanager/bioprograms/execute/ExecuteException.java
@@ -0,0 +1,18 @@
+package org.genomesmanager.bioprograms.execute;
+
+public class ExecuteException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ExecuteException(String arg0) {
+        super(arg0);
+    }
+
+    public ExecuteException(Exception e) {
+        super(e);
+    }
+
+    public ExecuteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
